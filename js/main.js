eb17d31f251be4133989a2d3ceca3fa9ca4c5eb9
richard-boss <soliyevabdulloh2005@gmail.com>
2022-11-27 23:33:30 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var elTitle = document.querySelector ('.title')
+
+
+var userName = prompt('Ismiz nmedi🤔', 'ABDULLOH😎')
+
+var ticketPrise = 500;
+var hotelPrice = 250;
+var othersPrice = 120;
+var dolarToSum = 11225;
+var euroToSum = 11686;
+var dollar = (ticketPrise + hotelPrice) * dolarToSum;
+var euro = othersPrice * euroToSum;
+var owerAll = dolar + euro;
+var userMoney = prompt(`To'risini etin nechi puliz bor ${userName}aka 💰`);
+if (userMoney >= owerAll ) {
+  elTitle.textContent = `Oq yo'l ${userName}aka GAZINI BOSING✊`;
+} else {
+  elTitle.textContent = `${userName}aka hisobingizda yetarlik mablag mavjud emas, Iltimos yana biroz pulni bosing😉`;
+}
